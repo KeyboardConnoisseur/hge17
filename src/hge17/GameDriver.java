a86ced98ea8021cf8cb6805208e76227dda5dae6
Gage Boyd <rageagainstthevm@gmail.com>
2017-04-02 20:42:01 -0500

    set up the project

---
+++ src/hge17/GameDriver.java
@@ -0,0 +1,43 @@
+package hge17;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class GameDriver extends BasicGame
+{
+	public GameDriver(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 10, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new GameDriver("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(GameDriver.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
